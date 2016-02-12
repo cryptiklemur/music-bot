52fdcec9661eefff3bb81c6cdd595a8852208038
Aaron Scherer <aequasi@gmail.com>
2016-02-12 13:48:41 -0800

    Adding vagrant

---
+++ src/Command/StopCommand.js
@@ -0,0 +1,25 @@
+const AbstractCommand = require('discord-bot-base').AbstractCommand;
+
+class PauseCommand extends AbstractCommand {
+    static get name() { return 'pause'; }
+
+    static get description() { return 'Pauses the playback from the bot'; }
+
+    initialize() {
+        this.helper = this.container.get('helper.playback');
+    }
+
+    handle() {
+        this.responds(/^pause$/, () => {
+            if (!this.helper.isPlaying()) {
+                return this.reply("I am not playing music!");
+            }
+
+            this.helper.pause(() => {
+                this.sendMessage(this.message.channel, "Playback has been paused.");
+            });
+        });
+    }
+}
+
+module.exports = PauseCommand;
